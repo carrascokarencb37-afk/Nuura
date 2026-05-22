c9626c5b2743d8644edbbf2df6e478ff1810ed04
Julissa <espinoza.julissa.cb37@gmail.com>
2026-05-22 15:32:43 -0700

    wea de base de datos

--- .history/script/sesion_20260522152518.js
+++ .history/script/sesion_20260522152518.js
@@ -1,7 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
 
 const supabaseUrl = 'https://trfupybydxgmcozqwpak.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyZnVweWJ5ZHhnbWNvenF3cGFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MTgzNDcsImV4cCI6MjA5NDM5NDM0N30.SNexC4W6M6Tz78g-0IE_STfhhweA37ykcqruSaT2n_M';
+const supabaseKey = 'sb_publishable_CxM4HostaaVYhh8uIXM8Qw_U3r3ccTR';
 
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 

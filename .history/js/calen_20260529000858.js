5d5e469081421357ca55a056942270e4a3c40e6f
Julissa <espinoza.julissa.cb37@gmail.com>
2026-05-29 00:15:17 -0700

    calendario2

--- .history/js/calen_20260529000858.js
+++ .history/js/calen_20260529000858.js
@@ -2,17 +2,14 @@ element.classList.add("riego");
 
 //es como decir, regar cada X días
 const flores = {
-    rosa: 5,
-    jazmin: 1,
-    
     corona: 3,
     duranta: 2,
-
+    rosa: 5,
     pervinca: 1,
     capuchina: 1,
     ruda: 2,
     trompillo: 1,
-
+    jazmin: 1,
     pascua: 2,
     bigotillo: 2
 };
